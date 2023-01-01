9130d17e22dc74e0bb8de64b35c4633a84f84e15
alecejones-noodoe <alecejones@noodoe.com>
2023-01-01 15:45:51 +0800

    fix: upgrade node types version

--- src/config/plugin.ts
+++ src/config/plugin.ts
@@ -1,20 +1,21 @@
-import { PluginJsonConfiguration } from 'wordparrot-types'
+import { PluginJsonConfiguration } from 'wordparrot-types';
 
 const PluginJson: PluginJsonConfiguration = {
-    name: "plugin-starter",
-    title: "",
-    description: "",
-    private: false,
-    hubPluginId: "",
-    platform: "",
-    website: "",
-    version: "0.0.0",
-    repository: "",
-    author: "author",
-    license: "",
-    logo: "",
-    categories: [],
-    subcategories: []
-}
+  name: 'plugin-starter',
+  title: '',
+  description: '',
+  private: false,
+  hubPluginId: '',
+  platform: '',
+  website: '',
+  version: '0.0.0',
+  sourceVersion: '',
+  repository: '',
+  author: 'author',
+  license: '',
+  logo: '',
+  categories: [],
+  subcategories: [],
+};
 
-export default PluginJson
+export default PluginJson;
