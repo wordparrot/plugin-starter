a21180a7a05229504d08f59720be57506d604dab
alecejones-noodoe <alecejones@noodoe.com>
2023-03-30 15:35:25 -0500

    fix: add lib folder

--- src/lib/functions.ts
+++
@@ -1,8 +0,0 @@
-export const getMimeType = (extension: string) => {
-    switch(extension) {
-        case 'csv':
-            return 'text/csv'
-        default:
-            throw new Error('mimetype_not_supported')
-    }
-}
