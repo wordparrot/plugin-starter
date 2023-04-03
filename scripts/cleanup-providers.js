697c66b2d04fb0d1830b6fd6c4fb6ec6b414dcb3
alecejones-noodoe <alecejones@noodoe.com>
2023-04-02 21:25:58 -0500

    fix: remove extraneous js files

--- scripts/cleanup-providers.js
+++ scripts/cleanup-providers.js
@@ -3,6 +3,7 @@
 const fs = require('fs');
 const util = require('util');
 const unlinkPromisified = util.promisify(fs.unlink);
+const FileHound = require('filehound');
 
 const filenames = [
   './src/actions/providers.js',
@@ -17,6 +18,22 @@ return Promise.all(
   filenames.map((filename) => {
     return unlinkPromisified(filename);
   })
-).catch(() => {
-  console.log('Cleanup providers: One or more providers files was not found.');
-});
+)
+  .then(() => {
+    return FileHound.create()
+      .glob(['*.formconfig.js'])
+      .find()
+      .then((formConfigFilenames) => {
+        return Promise.all(
+          formConfigFilenames.map((filename) => {
+            return unlinkPromisified(filename);
+          })
+        );
+      });
+  })
+  .then(() => {
+    console.log('extraneous js files deleted.');
+  })
+  .catch(() => {
+    console.log('Cleanup providers: One or more providers files was not found.');
+  });
